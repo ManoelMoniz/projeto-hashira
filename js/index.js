d7c97a1caf1602a54d0412b96b3b6714176d3b88
Manoel Fernando Moniz Filho <mmoniz.au@gmail.com>
2024-08-01 20:02:12 -0300

    final

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 const botoes = document.querySelectorAll(".botao");
+
 const hashiras = document.querySelectorAll(".hashira");
+
 botoes.forEach((botao, indice) => {
     botao.addEventListener("click", () => {
 
-        const botaoSelecionado = document.querySelector(".botaoselecionado");
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
 
         botaoSelecionado.classList.remove("selecionado");
 
         botao.classList.add("selecionado");
 
-        const hashiraSelecionado = document.querySelector(".hashiraselecionado");
+        const hashiraSelecionado = document.querySelector(".hashira.selecionado");
 
         hashiraSelecionado.classList.remove("selecionado");
 
